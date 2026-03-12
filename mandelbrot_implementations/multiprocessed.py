9bf90a5b904f685d9ebfc1f55041e787ad4949f2
Gvidas Rimkus <gvidasrimkus88@gmail.com>
2026-03-12 19:06:43 +0100

    MP2 M1: chunked implementation done and tested

--- mandelbrot_implementations/multiprocessed.py
+++ mandelbrot_implementations/multiprocessed.py
@@ -6,22 +6,25 @@ import time
 import math
 import os
 
-def generate_set(resolution=1024, x_min=-2, x_max=1, y_min=-1.5, y_max=1.5, n_workers=6):
-    chunk_size = max(1, resolution // n_workers)
+def generate_set(resolution=1024, x_min=-2, x_max=1, y_min=-1.5, y_max=1.5, n_workers=6, n_chunks=None):
+    if n_chunks is None:
+        n_chunks = n_workers
+    chunk_size = max(1, resolution // n_chunks)
     chunks, row = [], 0
     while row < resolution:
         row_end = min(row + chunk_size, resolution)
         chunks.append((row, row_end, resolution, x_min, x_max, y_min, y_max))
         row = row_end
+    tiny = [(0, 8, 8, x_min, x_max, y_min, y_max)]
     with Pool(processes=n_workers) as pool:
-        pool.map(_worker, chunks)
+        pool.map(_worker, tiny)#warm up
         parts = pool.map(_worker, chunks)
     return np.vstack(parts)
 
 def _worker(args):
     return generate_subset(*args)
 
-@njit(fastmath=True)
+@njit(fastmath=True, cache=True)
 def generate_subset(row_start, row_end, resolution, x_min, x_max, y_min, y_max):
     x_region = np.linspace(start=x_min, stop=x_max, num=resolution)
     y_region = np.linspace(start=y_min, stop=y_max, num=resolution)
@@ -32,7 +35,7 @@ def generate_subset(row_start, row_end, resolution, x_min, x_max, y_min, y_max):
             grid[i][j] = n
     return grid
 
-@njit(fastmath=True)
+@njit(fastmath=True, cache=True)
 def evaluate_point(x:float, y:float, max_iter:int):
     c = x + 1j*y
     z = 0
@@ -45,6 +48,7 @@ def evaluate_point(x:float, y:float, max_iter:int):
     return n
 
 if __name__ == "__main__":
+    n_workers = 12
     resolution = 2048
     x_min=-2 
     x_max=1
@@ -57,15 +61,38 @@ if __name__ == "__main__":
         times.append(time.perf_counter() - t0)
     t_serial = statistics.median(times)
 
-    for n_workers in range(1, os.cpu_count()+1):
-        chunk_size = max(1, resolution // n_workers)
+    #Here we do not use the generate_set function, due to the requirement of not including the creation of workers (and the like) in the timing.
+    # for n_workers in range(1, os.cpu_count()+1):
+    #     chunk_size = max(1, resolution // n_workers)
+    #     chunks, row = [], 0
+    #     while row < resolution:
+    #         row_end = min(row + chunk_size, resolution)
+    #         chunks.append((row, row_end, resolution, x_min, x_max, y_min, y_max))
+    #         row = row_end
+    #     tiny = [(0, 8, 8, x_min, x_max, y_min, y_max)]            
+    #     with Pool(processes=n_workers) as pool:
+    #         pool.map(_worker, tiny) #warm up
+    #         times = []
+    #         for _ in range(10):
+    #             t0 = time.perf_counter()
+    #             np.vstack(pool.map(_worker, chunks))
+    #             times.append(time.perf_counter() - t0)
+    #         t_par = statistics.median(times)
+    #         std = np.std(times)
+    #         ci = 1.96 * std / math.sqrt(10)
+    #         speedup = t_serial / t_par
+    #         print(f"{n_workers:2d} workers: {t_par:.5f} ± {ci:.5f}s, speedup={speedup:.2f}x, eff={speedup/n_workers*100:.0f}%")
+    chunk_counts = [n_workers * multiple for multiple in [4]]
+    for chunk_count in chunk_counts:
+        chunk_size = max(1, resolution // chunk_count)
         chunks, row = [], 0
         while row < resolution:
             row_end = min(row + chunk_size, resolution)
             chunks.append((row, row_end, resolution, x_min, x_max, y_min, y_max))
             row = row_end
+        tiny = [(0, 8, 8, x_min, x_max, y_min, y_max)]            
         with Pool(processes=n_workers) as pool:
-            pool.map(_worker, chunks) #warm up
+            pool.map(_worker, tiny) #warm up
             times = []
             for _ in range(10):
                 t0 = time.perf_counter()
@@ -75,7 +102,7 @@ if __name__ == "__main__":
             std = np.std(times)
             ci = 1.96 * std / math.sqrt(10)
             speedup = t_serial / t_par
-            print(f"{n_workers:2d} workers: {t_par:.5f} ± {ci:.5f}s, speedup={speedup:.2f}x, eff={speedup/n_workers*100:.0f}%")
+            print(f"{chunk_count} chunks, {n_workers:2d} workers: {t_par:.5f} ± {ci:.5f}s, speedup={speedup:.2f}x, eff={speedup/n_workers*100:.0f}%")
 
 #  resolution = 1024
 #  1 workers: 0.11720 ± 0.00077s, speedup=0.92x, eff=92%
@@ -89,7 +116,7 @@ if __name__ == "__main__":
 #  9 workers: 0.03274 ± 0.00113s, speedup=3.31x, eff=37%
 # 10 workers: 0.03036 ± 0.00160s, speedup=3.57x, eff=36%
 # 11 workers: 0.02884 ± 0.00160s, speedup=3.76x, eff=34%
-# 12 workers: 0.02739 ± 0.00136s, speedup=3.95x, eff=33%
+# 12 workers: 0.02739 ± 0.00136s, speedup=3.95x, eff=33% Fastest
 
 #  resolution = 2048
 #  1 workers: 0.46752 ± 0.00903s, speedup=0.91x, eff=91%
@@ -102,5 +129,9 @@ if __name__ == "__main__":
 #  8 workers: 0.13181 ± 0.00152s, speedup=3.24x, eff=41%
 #  9 workers: 0.12810 ± 0.00205s, speedup=3.34x, eff=37%
 # 10 workers: 0.11751 ± 0.00488s, speedup=3.64x, eff=36%
-# 11 workers: 0.10688 ± 0.00417s, speedup=4.00x, eff=36%
+# 11 workers: 0.10688 ± 0.00417s, speedup=4.00x, eff=36% Fastest
 # 12 workers: 0.10937 ± 0.00411s, speedup=3.91x, eff=33%
+
+# MP2 M1: 
+#   res 1024: 48 chunks, 12 workers: 0.02479 ± 0.00096s, speedup=4.37x, eff=36%
+#   res 2048: 48 chunks, 12 workers: 0.09287 ± 0.00303s, speedup=4.57x, eff=38%
